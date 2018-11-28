bc8deb34fc8ca0d32560f49d66866a93df542467
chenhaibo <chenhaibo001@ke.com>
2018-11-28 19:49:04 +0800

    client add zk username and password

--- grpc-server/src/main/java/com/chenhaibo/dao/UserMapper.java
+++ grpc-server/src/main/java/com/chenhaibo/dao/UserMapper.java
@@ -2,6 +2,7 @@ package com.chenhaibo.dao;
 
 import com.chenhaibo.model.User;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * <p>
@@ -11,6 +12,7 @@ import com.baomidou.mybatisplus.mapper.BaseMapper;
  * @author chenhaibo
  * @since 2018-11-27
  */
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
 
     User findByUserName(String name);
